87b3782f00743ece4007439757574e62c8c0e3d3
AKBAR MOHAMMAD <43230334+AKBARMOHAMMAD@users.noreply.github.com>
2019-02-26 17:41:41 +0530

    Update settings.py

--- Rest_api 2/sample/settings.py
+++ Rest_api 2/sample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h!#xg-z0gvhasdg3t6r#*ktry7v$=h77kxdpy^xc933$+(!(z)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.3']
 
 # Application definition
 
